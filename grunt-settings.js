3e2d52890d36decbb755db4761c04ef925551aa2
Marmik Desai <marmikdesai@live.com>
2016-06-27 15:44:10 +0530

    less html
    less html

---
+++ grunt-settings.js
@@ -0,0 +1,17 @@
+module.exports = {
+    lessDir:          'less/',
+    lessMainFileName: 'main',
+    cssDir:           'css/',
+    cssMainFileDir:   'css/',
+    cssMainFileName:  'main',
+    jsDir:            'js/',
+    imgDir:           'images/',
+    imgSourceDir:     'sourceimages/',
+
+    // sftp server
+    sftpServer:       'example.com',
+    sftpPort:         '2121',
+    sftpLogin:        'login',
+    sftpPas:          'password',
+    sftpDestination:  '/pathTo/css'
+  };
